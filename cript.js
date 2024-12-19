6961af056e9dd340cda6e7f5b0fd430284714736
Andre Brondi <96538368+andredbrondi@users.noreply.github.com>
2024-12-19 14:45:19 -0300

    Create cript.js

---
+++ cript.js
@@ -0,0 +1,8 @@
+// Adiciona um efeito ao botão ao clicar
+document.querySelectorAll('.cta-button').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Acesso liberado em 25/11, 19h!');
+  });
+});
+
+  
